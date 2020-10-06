8097a3f1e5cc8a12cd11f1215755cf7894e55761
doosemavis <moosedavis2011@gmail.com>
2020-10-06 18:03:21 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -12,26 +12,27 @@ class Student
   end 
   
   def self.create_table
-    sql = <<- SQL 
+    sql = <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
       )
       SQL
+
       DB[:conn].execute(sql)
   end 
 
   def self.drop_table
-    sql = <<- SQL
-    DROP TABLE students
-    SQL 
+    sql = <<-SQL
+      DROP TABLE students
+      SQL 
 
-    DB[:conn].execute(sql)
-  end 
+      DB[:conn].execute(sql)
+  end  
 
   def save
-    sql = <<- SQL 
+    sql = <<-SQL 
       INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
@@ -41,7 +42,10 @@ class Student
     @id = DB[:conn].execute("SELECT last_inserted_rowid() FROM students")[0][0]
   end 
 
-  def self.create
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
   end 
 
 end
